af06771d42b37171ade96357128bbe3790f5d5a2
huytq <huytq@ominext.com>
2022-10-21 17:29:44 +0700

    pull to refresh, background AM/PM

--- lib/bloc/home/home_bloc.dart
+++ lib/bloc/home/home_bloc.dart
@@ -12,7 +12,7 @@ class HomeBloc extends Bloc<HomeEvent, HomeState> {
   HomeBloc({required this.weatherRepository, required this.locationRepository})
       : super(HomeStateInitialized()) {
     on<HomeLoaded>((_, emit) => _onGetHomeData(emit));
-    on<HomePullToRefresh>((_, emit) => _onGetHomeData(emit));
+    on<HomePullToRefresh>((_, emit) => _onRefreshHomeData(emit));
   }
 
   void _onGetHomeData(Emitter<HomeState> emitter) async {
@@ -46,6 +46,37 @@ class HomeBloc extends Bloc<HomeEvent, HomeState> {
             days: weathers.map((e) => DayWeather(e)).toList()),
       );
     }
+  }
+
+  void _onRefreshHomeData(Emitter<HomeState> emitter) async {
+    final response = await weatherRepository.getWeatherData("");
+    final currentConditions = response.currentConditions;
+    final weathers = response.weathers;
+    final areas = response.nearestAreas;
+    if (currentConditions.isEmpty || weathers.isEmpty || areas.isEmpty) {
+      return;
+    }
+    final condition = currentConditions.first;
+    final cities = await locationRepository.getPlaceMark(
+        areas.first.latitude, areas.first.longitude);
+    final hourlyWeathers = weathers
+        .expand(
+          (day) => day.hourlyWeathers.map((e) {
+            return e..dayValue = DateTime.parse(day.date).day;
+          }),
+        )
+        .toList()
+      ..sort((a, b) => a.compareTo(b));
+    final sortedWeathers =
+        _getSortedHourlyWeatherList(hourlyWeathers).take(8).toList();
+    emitter(
+      HomeLoadSuccess(
+          currentCondition: condition,
+          dayWeathers: weathers,
+          hourWeathers: sortedWeathers,
+          area: areas.first..cities = cities,
+          days: weathers.map((e) => DayWeather(e)).toList()),
+    );
   }
 
   List<Hourly> _getSortedHourlyWeatherList(List<Hourly> weathers) {
